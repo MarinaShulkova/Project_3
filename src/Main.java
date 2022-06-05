433d1d53be42081f217c28b828a354f7fa2ace4f
Marash_79@mail.ru <MarinaShulkova>
2022-06-05 23:31:14 +0300

    Add 3 and 4

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import java.util.*;
 
 public class Main {
     static List<Flat> flats = new ArrayList<Flat>();
+    static List<Estate> estates = new ArrayList<Estate>();
 
     public static void main(String[] args) {
         setFlats();
         menu(print_menu());
+
+
     }
 
     public static void menu(int menu_item) {
         switch (menu_item) {
             case 1:  //добавление объекта
-                System.out.println("Choose property: \n 1.Flat \n 2.Commercial real estate");
-                Scanner scr = new Scanner(System.in);
-                if (scr.nextInt() == 1) {
+                    if (menu_2() == 1) {
                     Flat f = new Flat("f", "f", "f", 5, 55, 55000,5);
                     flats.add(f);
                     flats.add(addProperty(1));
@@ -25,8 +26,17 @@ public class Main {
             case 2:  //удаление объекта
                 flats.remove(1);
             case 3:  //сорти5ровка по возрастанию
-
+                if (menu_2() == 1) {
+                flats.stream().sorted(Comparator.comparing(Flat::getPrice)).forEach(System.out::println);}
+                else {
+                    estates.stream().sorted(Comparator.comparing(Estate::getPrice)).forEach(System.out::println);}
+                menu(print_menu());
             case 4:  //сортировка по убыванию
+                if (menu_2() == 1) {
+                flats.stream().sorted(Comparator.comparing(Flat::getPrice).reversed()).forEach(System.out::println);}
+                else {
+                    estates.stream().sorted(Comparator.comparing(Estate::getPrice).reversed()).forEach(System.out::println);}
+                    menu(print_menu());
             case 5:  //вывести квартиру с наивысшей оценкой
             case 6:  //печать списка квартир на продажу
                 flats.stream().forEach(System.out::println);
@@ -58,7 +68,6 @@ public class Main {
         System.out.println("Choose menu item: ");
         Scanner scr = new Scanner(System.in);
         int menu_item = scr.nextInt();
-
         return menu_item;
     }
 
@@ -99,19 +108,26 @@ public class Main {
     }
 
     public static void setFlats(){
-             flats.add(new Flat("Flat 1", "Pervomayskaya", "9 Parkovaya 36", 2, 47.3, 10200000, 4.2F));
-    flats.add(new Flat("Flat 2", "Strogino", "Stroginskaya 12", 2, 68, 15000000, 4.7F));
+        flats.add(new Flat("Flat 1", "Pervomayskaya", "9 Parkovaya 36", 2, 47.3, 10200000, 4.2F));
+        flats.add(new Flat("Flat 2", "Strogino", "Stroginskaya 12", 2, 68, 15000000, 4.7F));
         flats.add(new Flat("Flat 3", "Domodedovskaya", "Domodedovskaya 58", 3, 105.8, 23000000, 4.8F));
         flats.add(new Flat("Flat 4", "Tverskaya", "2 Tverskaya-Yamskya", 1, 13.2, 25000000, 3.5F));}
 
- /*   static List<Estate> estates = Arrays.asList(
-            new Estate("Estate 1", "Tulskaya", "Tulskaya 42", "office", 87, 18328000, 3.2F),
-            new Estate("Estate 2", "Altufevo", "Leskova 52/1", "office", 125.8, 27000000, 4.9F),
-            new Estate("Estate 3", "Kuzminki", "Zelenodolskaya 35", "kiosk", 15.3, 5236000, 1.8F),
-            new Estate("Estate 4", "Ohotnyi ryad", "Krasnaya ploschad 9", "mausoleum", 220, 138000000, 4.28F),
-            new Estate("Estate 5", "Krasnoselskaya", "Krasnoselskaya 3a", "shop", 73, 42180000, 4.5F));
-*/
+    public static void setEstates(){
+        estates.add(new Estate("Estate 1", "Tulskaya", "Tulskaya 42", "office", 87, 18328000, 3.2F));
+        estates.add(new Estate("Estate 2", "Altufevo", "Leskova 52/1", "office", 125.8, 27000000, 4.9F));
+        estates.add(new Estate("Estate 3", "Kuzminki", "Zelenodolskaya 35", "kiosk", 15.3, 5236000, 1.8F));
+        estates.add(new Estate("Estate 4", "Ohotnyi ryad", "Krasnaya ploschad 9", "mausoleum", 220, 138000000, 4.28F));
+        estates.add(new Estate("Estate 5", "Krasnoselskaya", "Krasnoselskaya 3a", "shop", 73, 42180000, 4.5F));}
+
+    public static int menu_2 () {
+        System.out.println("Choose property: \n 1.Flat \n 2.Commercial real estate");
+        Scanner scr = new Scanner(System.in);
+        int menu_3 = scr.nextInt();
+        return menu_3;
+    }
 }
+
 
 
 
